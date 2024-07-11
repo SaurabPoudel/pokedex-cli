f10e8491a1a85c2b240aa16809edfded45d21f19
SaurabPoudel <poudelsaurab20@gmail.com>
2024-07-11 07:57:24 +0545

    repl

---
+++ command_help.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func callbackHelp() error {
+	fmt.Println("Welcome to the Pockedex help menu!")
+	fmt.Println("Here are your available commands:")
+
+	avaliableCommands := getCommands()
+	for _, cmd := range avaliableCommands {
+		fmt.Printf("- %s: %s \n", cmd.name, cmd.description)
+	}
+	fmt.Println("")
+	return nil
+}
