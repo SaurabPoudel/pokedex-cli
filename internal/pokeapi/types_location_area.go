96911836a6c798a1e511f9c0dea41919251055b2
SaurabPoudel <poudelsaurab20@gmail.com>
2024-07-13 19:03:05 +0545

    map , mapb and pokecachetest

---
+++ internal/pokeapi/types_location_area.go
@@ -0,0 +1,12 @@
+package pokeapi
+
+// previous may be null so we put pointer there
+type LocationAreaResp struct {
+	Count    int     `json:"count"`
+	Next     *string `json:"next"`
+	Previous *string `json:"previous"`
+	Results  []struct {
+		Name string `json:"name"`
+		URL  string `json:"url"`
+	} `json:"results"`
+}
